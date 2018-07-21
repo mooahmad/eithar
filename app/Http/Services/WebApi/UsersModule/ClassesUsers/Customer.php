e77281feb24641ff15b495ac22967f83db5c7c23
ashrfhassan <ashrfhassan890@gmail.com>
2018-07-21 13:31:07 +0200

    uploading customer images

---
+++ app/Http/Services/WebApi/UsersModule/ClassesUsers/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ashHassan
+ * Date: 7/21/18
+ * Time: 12:13 PM
+ */
+
+namespace App\Http\Services\WebApi\UsersModule\ClassesUsers;
+
+
+class Customer
+{
+
+}
