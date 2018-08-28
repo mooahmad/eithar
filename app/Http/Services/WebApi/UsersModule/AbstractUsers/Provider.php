4da8c9764185aa0157adae25273a5ee6c1f36904
DESKTOP-RGO4RQB\HUD Systems <ahassan@hudsystems.com>
2018-08-28 14:51:39 +0300

    updating calendar

--- app/Http/Services/WebApi/UsersModule/AbstractUsers/Provider.php
+++ app/Http/Services/WebApi/UsersModule/AbstractUsers/Provider.php
@@ -14,10 +14,14 @@ class Provider
     public function getProvider($request, $providerId)
     {
         $day = $request->input('day');
-        if(empty($day))
-            $day = Carbon::today()->format('Y-m-d');
         $provider = ProviderModel::where('id', $providerId)->with(['calendar' => function ($query) use ($day) {
-            $query->where('providers_calendars.start_date', 'like', "%$day%");
+            if(empty($day)) {
+                $day = Carbon::today()->format('Y-m-d H:m:s');
+                $query->where('providers_calendars.start_date', '>=', "%$day%");
+            }else{
+                $day = Carbon::today()->format('Y-m-d');
+                $query->where('providers_calendars.start_date', 'like', "%$day%");
+            }
         }])->first();
         return Utilities::getValidationError(config('constants.responseStatus.success'),
                                              new MessageBag([
