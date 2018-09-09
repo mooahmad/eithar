092d3ef4012b1f75bd9c81f880d36e289fc41167
AymanZayed <azayed@hudsystems.com>
2018-09-09 16:46:13 +0200

    show family meeber details in invoice

--- app/Models/FamilyMember.php
+++ app/Models/FamilyMember.php
@@ -13,4 +13,12 @@ class FamilyMember extends Model
     protected $table = 'family_members';
     protected $dateFormat = 'Y-m-d H:m:s';
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->middle_name} {$this->last_name}";
+    }
 }
