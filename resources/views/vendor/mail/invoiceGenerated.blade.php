ec81305492f28c1e717e57b3fad9b0ee95c4daab
AymanZayed <azayed@hudsystems.com>
2018-09-30 16:17:06 +0200

    add email new template

---
+++ resources/views/vendor/mail/invoiceGenerated.blade.php
@@ -0,0 +1,16 @@
+@extends('vendor.mail.layouts.master')
+
+@section('content')
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 40px 40px 20px; text-align: center;">
+            <h1 style="margin: 0; font-family: sans-serif; font-size: 24px; line-height: 27px; color: #333333; font-weight: normal;">dedede</h1>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 0 40px 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; text-align: center;">
+            <p style="margin: 0;">
+                fffff
+            </p>
+        </td>
+    </tr>
+@endsection
---
+++ resources/views/vendor/mail/layouts/footer.blade.php
@@ -0,0 +1,70 @@
+<!-- Clear Spacer : BEGIN -->
+<tr>
+    <td aria-hidden="true" height="40" style="font-size: 0; line-height: 0;">
+        &nbsp;
+    </td>
+</tr>
+
+</table>
+
+<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="600" style="margin: auto;" class="email-container">
+    <!-- 1 Column Text : BEGIN -->
+    <tr>
+        <td bgcolor="#ffffff">
+            <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                <tr>
+                    <td style="padding: 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+                        ConsulTrust is highly secure and easy to use platform where clients can visit consultants while home. All our consultants are professional and certified in their areas. ConsulTrust is seeking to make difference in your life. Try the service, enjoy the experience and change your life.
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 0 40px 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+            <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto">
+                <tr>
+                    <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                        <a href="https://itunes.apple.com/us/app/consultrust/id1279557196?ls=1&mt=8" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                            &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff;">Apple Store</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            <!-- Button : END -->
+        </td>
+    </tr>
+
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 0 40px 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+            <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto">
+                <tr>
+                    <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                        <a href="https://play.google.com/store/apps/details?id=com.hudsystems.consulTrust" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                            &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff;">Google Store</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            <!-- Button : END -->
+        </td>
+    </tr>
+    <!-- 1 Column Text : END -->
+
+</table>
+<!-- Email Body : END -->
+
+<!-- Email Footer : BEGIN -->
+<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="600" style="margin: auto; font-family: sans-serif; color: #888888; line-height:18px;" class="email-container">
+    <tr>
+        <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; line-height:18px; text-align: center; color: #888888;" class="x-gmail-data-detectors">
+            {{--<webversion style="color:#cccccc; text-decoration:underline; font-weight: bold;">View as a Web Page</webversion>--}}
+            Copyright &copy 2017 HUD Systems LTD<br>Mayfield House, 14 Rochfords Gardens, Slough, England, SL2 5XA<br>support@consultrustpro.com
+        </td>
+    </tr>
+</table>
+<!-- Email Footer : END -->
+</center>
+</body>
+</html>
