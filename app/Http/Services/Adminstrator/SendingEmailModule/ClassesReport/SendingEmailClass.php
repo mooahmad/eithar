56ad0cf15358005438a417c7c0a515d612d9e74d
AymanZayed <azayed@hudsystems.com>
2018-10-02 15:08:46 +0200

    send email when add new item to invoice

--- app/Http/Services/Adminstrator/SendingEmailModule/ClassesReport/SendingEmailClass.php
+++ app/Http/Services/Adminstrator/SendingEmailModule/ClassesReport/SendingEmailClass.php
@@ -4,6 +4,7 @@ namespace App\Http\Services\Adminstrator\SendingEmailModule\ClassesReport;
 
 
 use App\Helpers\Utilities;
+use App\Mail\Invoice\AddItemToInvoice;
 use App\Mail\Invoice\GenerateInvoice;
 use App\Models\MedicalReports;
 use Illuminate\Http\Request;
@@ -16,16 +17,26 @@ class SendingEmailClass
 
     /**
      * @param $customer
-     * @param $notification_data
+     * @param $notification
      * @return bool
      */
-    public static function prepareEmail($customer,$notification_data)
+    public static function prepareEmail($customer,$notification)
     {
+        $notification_data = json_decode(json_encode($notification->data));
         if (!$notification_data->notification_type || $notification_data->notification_type<1 || $notification_data->notification_type>7) return false;
 
         if ($notification_data->notification_type == config('constants.pushTypes.invoiceGenerated')){
             try{
                 Mail::to($customer)->send(new GenerateInvoice($customer,$notification_data));
+                return true;
+            }catch (\Exception $exception){
+                return false;
+            }
+        }
+
+        if ($notification_data->notification_type == config('constants.pushTypes.addItemToInvoice')){
+            try{
+                Mail::to($customer)->send(new AddItemToInvoice($customer,$notification_data));
                 return true;
             }catch (\Exception $exception){
                 return false;
