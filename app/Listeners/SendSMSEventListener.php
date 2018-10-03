72471e7a9ba8557194dd293edccaace858bc2b52
AymanZayed <azayed@hudsystems.com>
2018-10-03 14:28:28 +0200

    send sms message using mobily

--- app/Listeners/SendSMSEventListener.php
+++ app/Listeners/SendSMSEventListener.php
@@ -2,15 +2,14 @@
 
 namespace App\Listeners;
 
-use App\Events\SendEmailsEvent;
-use App\Helpers\Utilities;
-use App\Http\Services\Adminstrator\SendingEmailModule\ClassesReport\SendingEmailClass;
+use App\Events\SendSMSEvent;
+use App\Http\Services\Adminstrator\SendingSMSModule\ClassesReport\SendingSMSClass;
 use App\Models\Customer;
 use Carbon\Carbon;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendEmailsEventListener
+class SendSMSEventListener
 {
     /**
      * Create the event listener.
@@ -25,19 +24,21 @@ class SendEmailsEventListener
     /**
      * Handle the event.
      *
-     * @param  SendEmailsEvent $event
+     * @param  SendSMSEvent  $event
      * @return void
      */
-    public function handle(SendEmailsEvent $event)
+    public function handle(SendSMSEvent $event)
     {
         $now = Carbon::now()->format('Y-m-d H:m:s');
         $customers = Customer::all();
         $customers->each(function ($customer) use ($now) {
-            $customer->notifications()->where('is_emailed', 0)->latest()->get()->each(function ($notification) use ($now, $customer) {
+            $customer->notifications()->where('is_smsed', 0)->latest()->get()->each(function ($notification) use ($now, $customer) {
                 $data = json_decode(json_encode($notification->data));
                 if (strtotime($data->send_at) <= strtotime($now)) {
-                    if (SendingEmailClass::prepareEmail($customer, $notification)) {
-                        $notification->is_emailed = 1;
+                    $message  = $data->{'title_'.$data->lang} .'-'.$data->{'desc_'.$data->lang};
+                    $numbers = [$customer->mobile_number];
+                    if (SendingSMSClass::sendSMS($message,$numbers)) {
+                        $notification->is_smsed = 1;
                         $notification->save();
                     }
                 }
