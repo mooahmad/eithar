f96822852fe64dc44c29ef96f184c88836742f21
DESKTOP-RGO4RQB\HUD Systems <ahassan@hudsystems.com>
2018-12-10 11:20:51 +0200

    update close bookings time to next 10 hours

--- app/Http/Services/WebApi/UsersModule/AbstractUsers/Provider.php
+++ app/Http/Services/WebApi/UsersModule/AbstractUsers/Provider.php
@@ -1004,7 +1004,7 @@ class Provider
                             $startTime = Carbon::parse($calendar->start_date)->format('g:i A');
                             $endTime = Carbon::parse($calendar->end_date)->format('g:i A');
                             $now = Carbon::now();
-                            $nowPlusHours = Carbon::now()->addHours(3);
+                            $nowPlusHours = Carbon::now()->addHours(10);
                             $afterStartDay = Carbon::parse($calendar->start_date);
                             if (!($now <= $afterStartDay && $nowPlusHours >= $afterStartDay)) {
                                 $startDate = "Unknown";
@@ -1032,7 +1032,7 @@ class Provider
                             $startTime = Carbon::parse($calendar->start_date)->format('g:i A');
                             $endTime = Carbon::parse($calendar->end_date)->format('g:i A');
                             $now = Carbon::now();
-                            $nowPlusHours = Carbon::now()->addHours(3);
+                            $nowPlusHours = Carbon::now()->addHours(10);
                             $afterStartDay = Carbon::parse($calendar->start_date);
                             if (!($now <= $afterStartDay && $nowPlusHours >= $afterStartDay)) {
                                 $startDate = "Unknown";
@@ -1060,7 +1060,7 @@ class Provider
                         $startTime = Carbon::parse($calendar->start_date)->format('g:i A');
                         $endTime = Carbon::parse($calendar->end_date)->format('g:i A');
                         $now = Carbon::now();
-                        $nowPlusHours = Carbon::now()->addHours(3);
+                        $nowPlusHours = Carbon::now()->addHours(10);
                         $afterStartDay = Carbon::parse($calendar->start_date);
                         if (!($now <= $afterStartDay && $nowPlusHours >= $afterStartDay)) {
                             $startDate = "Unknown";
