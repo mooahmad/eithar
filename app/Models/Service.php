e181e96bce4fd55769920fe432feee08206b4989
DESKTOP-RGO4RQB\HUD Systems <ahassan@hudsystems.com>
2018-12-18 14:47:10 +0200

    fixing carbon issue

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\App;
 use App\Traits\ModelDateTimeAccessors;
+use Carbon\Carbon;
 
 class Service extends Model
 {
