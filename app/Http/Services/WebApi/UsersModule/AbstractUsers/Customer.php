0d5d4954578c0bd1f590182e9ecbb2d20f98949c
AymanElshehawy <elshehawy.m92@gmail.com>
2019-05-13 14:09:08 +0200

    fix sort by desc

--- app/Http/Services/WebApi/UsersModule/AbstractUsers/Customer.php
+++ app/Http/Services/WebApi/UsersModule/AbstractUsers/Customer.php
@@ -398,8 +398,8 @@ class Customer
                 }
             }
         }
-        $upCommingAppointments = collect($upCommingAppointments)->sortBy('start_date','Desc')->values()->all();
-        $passedAppointments = collect($passedAppointments)->sortBy('start_date','Desc')->reverse()->values()->all();
+        $upCommingAppointments = collect($upCommingAppointments)->sortBy('start_date',[],true)->values()->all();
+        $passedAppointments = collect($passedAppointments)->sortBy('start_date',[],true)->reverse()->values()->all();
         $appointments = collect(array_merge($upCommingAppointments, $passedAppointments));
         $appointments->each(function ($appointment) use (&$finalAppointments) {
             $appointment["start_date"] = Carbon::parse($appointment["start_date"])->format('l jS \\of F Y');
